ef2b37be0f86f1e61bdbe795fdad9f4dc0e3dbe7
xfkang <15658081582@163.com>
2022-05-07 14:03:35 +0800

    Initial commit

---
+++ app/src/main/java/com/itbird/bindview/v3/BindViewTestV3Activity.java
@@ -0,0 +1,40 @@
+package com.itbird.bindview.v3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itbird.bindview.R;
+import com.itbird.bindview.annotation.ItbirdAopBinderView;
+import com.itbird.bindview.annotation.ItbirdOnclick;
+
+/**
+ * 编译注解+APT+JAVAPoet+反射实现 view ioc框架
+ * 事件自动注册
+ */
+@ItbirdAopBinderView(R.layout.bindview_test)
+public class BindViewTestV3Activity extends AppCompatActivity {
+    private static final String TAG = BindViewTestV3Activity.class.getSimpleName();
+
+    @ItbirdAopBinderView(R.id.button)
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //关键的代码，通过反射，解析APT生成的类
+        BindViewImplV3.bind(this);
+    }
+
+    @ItbirdOnclick(R.id.button)
+    public void onItbirdClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Toast.makeText(this, "button on click", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
